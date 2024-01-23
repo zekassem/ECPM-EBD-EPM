f6cacffeb22bbb3b83e4ba7755dfd42ae5370d69
zekassem <86942431+zekassem@users.noreply.github.com>
2024-01-22 18:34:36 -0700

    Changes to the table

--- Summarizing_Results/EBD/SPC Formulation/Results/Summarizing_Results_Comp_Time.py
+++ Summarizing_Results/EBD/SPC Formulation/Results/Summarizing_Results_Comp_Time.py
@@ -7,30 +7,32 @@ Count_Infeasible=df_infeasible.groupby(['RN Name','tolerance']).agg({'RN Name':'
 Count_Infeasible.rename(columns={'Sol_Status_SP_Const':'No. of Infeasible Instances'}, inplace=True)
 
 df_time_limit=df[df['Sol_Status_SP_Const']=='time limit exceeded']
-gap_analysis=df_infeasible.groupby(['RN Name','tolerance']).agg({'gap':['mean','max']})
-gap_analysis.rename(columns={'gap':'Gap'}, inplace=True)
+gap_analysis=df_time_limit.groupby(['RN Name','tolerance']).agg({'RN Name':'first','tolerance':'first','Sol_Status_SP_Const':'count'})
+gap_analysis.rename(columns={'Sol_Status_SP_Const':'No. of Instance that Exceeded Time Limit'}, inplace=True)
+df_time_limit.to_csv('gap.csv',index=False)
 
-print(df_time_limit)
 
 
 df_feasible_time_not_exceeded=df[(df['Sol_Status_SP_Const']!='time limit exceeded') & (df['Sol_Status_SP_Const']!='CPLEX Error  1217: No solution exists.')]
-df['Total_Time_SP'] = pd.to_numeric(df['Total_Time_SP'], errors='coerce')
 Average_by_District = df_feasible_time_not_exceeded.groupby(['RN Name','No. of Districts']).agg({'RN Name':'first','No. of Districts':'first','Total_Time_SP':'mean'})
 Average_by_Tolerance = df_feasible_time_not_exceeded.groupby(['RN Name','tolerance']).agg({'RN Name':'first','tolerance':'first','Total_Time_SP':'mean'})
 Average_by_Tolerance=Average_by_Tolerance[['Total_Time_SP']]
 Count_Infeasible=Count_Infeasible[['No. of Infeasible Instances']]
+gap_analysis=gap_analysis[['No. of Instance that Exceeded Time Limit']]
 Average_by_Tolerance.reset_index(inplace=True)
 Count_Infeasible.reset_index(inplace=True)
+gap_analysis.reset_index(inplace=True)
 df_tolerance=pd.merge(Average_by_Tolerance,Count_Infeasible,on=['RN Name','tolerance'],how='left')
+df_tolerance=pd.merge(df_tolerance,gap_analysis,on=['RN Name','tolerance'],how='left')
 df_tolerance['No. of Infeasible Instances'] = df_tolerance['No. of Infeasible Instances'].fillna(0)
-latex_code = df_tolerance.to_latex(index=False, escape=False,formatters={'$|E|$': '{:,.0f}'.format,'$|V|$': '{:,.0f}'.format,'Total_Time_SP': '{:,.0f}'.format, 'No. of Infeasible Instances': '{:,.0f}'.format,'SPC-3 EBD': '{:,.0f}'.format})
+df_tolerance['No. of Instance that Exceeded Time Limit'] = df_tolerance['No. of Instance that Exceeded Time Limit'].fillna(0)
+df_tolerance['RN No.']=df_tolerance['RN Name'].apply(lambda x: 4 if x=='F6\_p' else 5)
+result_final=df_tolerance[['RN No.','tolerance','Total_Time_SP','No. of Infeasible Instances','No. of Instance that Exceeded Time Limit']]
+latex_code = result_final.to_latex(index=False, escape=False,formatters={'$|E|$': '{:,.0f}'.format,'$|V|$': '{:,.0f}'.format,'Total_Time_SP': '{:,.0f}'.format, 'No. of Infeasible Instances': '{:,.0f}'.format,'No. of Instance that Exceeded Time Limit': '{:,.0f}'.format})
 
-with open('dataframe_table_3.tex', 'w') as file:
+with open('dataframe_table_2.tex', 'w') as file:
     file.write(latex_code)
 
-latex_code_2 = Average_by_Tolerance.to_latex(index=False, escape=False,formatters={'$|E|$': '{:,.0f}'.format,'$|V|$': '{:,.0f}'.format,'Total_Time_SP': '{:,.0f}'.format, 'Branch_Cut_Time': '{:,.0f}'.format,'SPC-3 EBD': '{:,.0f}'.format})
-with open('dataframe_table_4.tex', 'w') as file:
-    file.write(latex_code_2)
 
 
 
--- Summarizing_Results/EBD/SPC Formulation/Results/Summarizing_results.py
+++ Summarizing_Results/EBD/SPC Formulation/Results/Summarizing_results.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('Summary_Branch_Cut_SPC_EBD.csv')
 df = df.dropna() # This line will remove the no solution rows from the dataframe
 df['No. of Instances']=27
-df['No. of Feasible Instances'] = df.groupby('No. of Nodes')['No. of Nodes'].transform('count')
+df['No. of Infeasible Instances'] = df['No. of Instances'] - df.groupby('No. of Nodes')['No. of Nodes'].transform('count')
 # Getting df with Exceeded Time Limit
 condition=df['Sol_Status_SP_Const']=='time limit exceeded'
 condition_df=df[condition]
@@ -14,23 +14,19 @@ Time_limit_Ex_df.reset_index(inplace=True)
 df=pd.merge(df,Time_limit_Ex_df,on='Instance_name',how='left')
 # Merge the original DataFrame with the grouped counts on 'No. of Nodes'
 df=df[df['Sol_Status_SP_Const']!='time limit exceeded']
-result = df.groupby('Instance_name').agg({'Instance_name':'first','No. of Nodes':'first','No. of Edges':'first','No. of Instances':'first','No. of Feasible Instances':'first','No. of Instances that Exceeded Time Limit':'first','Total_Time_SP': 'mean'})
+result = df.groupby('Instance_name').agg({'Instance_name':'first','No. of Nodes':'first','No. of Edges':'first','No. of Instances':'first','No. of Infeasible Instances':'first','No. of Instances that Exceeded Time Limit':'first','Total_Time_SP': 'mean'})
 result = result.reset_index(drop=True)
 result['NBV']=(result['No. of Nodes']*result['No. of Edges'])+result['No. of Nodes']
-result.rename(columns={'No. of Nodes': '$|V|$', 'No. of Edges': '$|E|$','Instance_name':'RN Name', 'Total_Time_SP':'SPC-3 EBD'}, inplace=True)
+result.rename(columns={'No. of Nodes': '$|V|$', 'No. of Edges': '$|E|$','Instance_name':'RN Name', 'Total_Time_SP':'EBD-SPC'}, inplace=True)
 result = result.sort_values(by='NBV')
 result['RN No.']=range(1, len(result) + 1)
 result['RN Name'] = result['RN Name'].str.replace('CARP_','').str.replace('_graph.dat', '').str.split('_').apply(lambda x: '\\_'.join(x))
-results_final_1=result[['RN No.','SPC-3 EBD']]
+results_final_1=result[['RN No.','EBD-SPC']]
 pd.options.display.float_format = '{:.2f}'.format
 # Convert DataFrame to LaTeX code
-latex_code = results_final_1.to_latex(index=False, escape=False,formatters={'$|E|$': '{:,.0f}'.format,'$|V|$': '{:,.0f}'.format,'NBV': '{:,.0f}'.format, 'Branch_Cut_Time': '{:,.0f}'.format,'SPC-3 EBD': '{:,.0f}'.format})
-# Save LaTeX code to a file
-with open('dataframe_table_1.tex', 'w') as file:
-    file.write(latex_code)
 
-results_final_2=result[['RN No.','No. of Instances','No. of Feasible Instances','No. of Instances that Exceeded Time Limit']]
-latex_code = results_final_2.to_latex(index=False, escape=False)
-with open('dataframe_table_2.tex', 'w') as file:
+results_final_2=result[['RN No.','EBD-SPC','No. of Instances','No. of Infeasible Instances','No. of Instances that Exceeded Time Limit']]
+latex_code = results_final_2.to_latex(index=False, escape=False,formatters={'EBD-SPC': '{:,.0f}'.format})
+with open('dataframe_table_1.tex', 'w') as file:
     file.write(latex_code)
 
